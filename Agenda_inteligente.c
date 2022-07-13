c99583bd2d3327a57abfb66d8c205d6db2cf51f7
Victor Hugo Amaral <amaral.vht@gmail.com>
2022-07-13 13:29:58 -0300

    Update Agenda_inteligente.c
    Corrigidos alguns erros no menu e um detalhe nas structs.
    
    Iniciada a leitura de arquivos e alocação dinâmica. (falta terminar)

--- Agenda_inteligente.c
+++ Agenda_inteligente.c
@@ -3,28 +3,41 @@
 #include <stdlib.h>
 
 // Struct Para musica
-struct musica{
-    int Samba;
-    int Rock;
-    int MPB;
-};
+typedef struct {
+    float sertanejo;
+    float rock;
+    float funk;
+} musica;
 
 // Struct para Informações gerais
-struct dados{
-    char Nome_completo[100];
-    char Nascimento[100];
-    char Cidade[100];
-    char UF[2];
-    struct musica esta_musica;
+typedef struct {
+    char nome_completo[100];
+    char nascimento[100];
+    char cidade[100];
+    char uf[2];
+    musica esta_musica;
     int grupo_registro;
-};
+} dados;
+
+
+dados *alcoa_pessoas(int n) {
+    dados *pessoas;
+
+    pessoas = calloc(n, sizeof(dados));
+    if(pessoas == NULL) {
+        printf("Erro ao alocar pessoas. \n");
+        return NULL;
+    }
+
+    return pessoas;
+}
 
 
 int main (){
     int opt;
     char nome_arquivo[50];
     FILE *arq_dados;
-    struct dados *pessoas;
+    dados *pessoas;
 
     do{
         // Menu
@@ -37,50 +50,59 @@ int main (){
         printf("[6] Ordenar registros\n");
         printf("[7] Agrupar registros\n");
         printf("[8] Exportar registros para um arquivo\n");
-        printf("[0] Agrupar registros\n");
+        printf("[0] Sair\n");
 
         scanf("%d", &opt);
-        switch (opt){
+        switch (opt) {
 
-        case 1:
-            //Recendo nome de arquivo e lendo arquivo
-            gets(nome_arquivo, 50, stdin);
-            arq_dados = fopen(nome_arquivo, "ab+");
+            case 1:
+                //Recendo nome de arquivo e lendo arquivo
+                fgets(nome_arquivo, 50, stdin);
 
-            if (arq_dados == NULL){
-                printf("Erro abrindo arquivo");
-            }
-            break;
-        case 2:
-         printf("b");
-            break;
-        case 3:
-            printf("c");
-            break;
-        case 4:
-            printf("d");
-            break;
-        case 5:
-            printf("e");
-            break;
-        case 6:
-            printf("f");
-            break;
-        case 7:
-            printf("g");
-            break;
-        case 8:
-            printf("h");
-            break;
-        case 0:
-            break;
-        default:
-            printf("Opção invalida!\n");
-            printf("Digite uma nova opção\n");
+                arq_dados = fopen(nome_arquivo, "ab+");
 
-            scanf("%d", &opt);
-            break;
+                if(arq_dados == NULL) {
+                    printf("Erro abrindo arquivo");
+                    exit(1);
+                }
+
+                // Aloca uma posicao para o vetor de structs "pessoas":
+                pessoas = aloca_pessoas(1);
+
+                // Falta terminar a leitura do arquivo:
+                // while(fread(pessoas, sizeof(dados), , arq_dados))
+
+                break;
+            case 2:
+                printf("b");
+                break;
+            case 3:
+                printf("c");
+                break;
+            case 4:
+                printf("d");
+                break;
+            case 5:
+                printf("e");
+                break;
+            case 6:
+                printf("f");
+                break;
+            case 7:
+                printf("g");
+                break;
+            case 8:
+                printf("h");
+                break;
+            case 0:
+                printf("Fechando a agenda. Ate mais!\n");
+                break;
+            default:
+                printf("Opção invalida!\n");
+                printf("Digite uma nova opção:\n");
+                break;
         }
-    }while(opt != 0);
+    } while(opt != 0);
+
     return 0;
 }
